edaf588ed5031db4ebfd938f988cf36c8fcb98c1
Safi Ahlberg <safi.ahlberg@gmail.com>
2018-07-11 16:04:53 +0200

    Added logging of complete SOAP Message

--- src/main/java/com/service/mm/mailbox/handler/DefaultHandler.java
+++ src/main/java/com/service/mm/mailbox/handler/DefaultHandler.java
@@ -4,9 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Set;
 
@@ -20,6 +23,16 @@ public class DefaultHandler implements SOAPHandler<SOAPMessageContext> {
 
     public boolean handleMessage(SOAPMessageContext context) {
         logger.info(isOutbound(context) ? "Message in outbound direction." : "Message in inbound direction.");
+
+        SOAPMessage message = context.getMessage();
+        try {
+            message.writeTo(System.out);
+        } catch (SOAPException e) {
+            logger.error("SOAPException during message.writeTo()", e);
+        } catch (IOException e) {
+            logger.error("IOException during message.writeTo()", e);
+        }
+
         return true;
     }
 
