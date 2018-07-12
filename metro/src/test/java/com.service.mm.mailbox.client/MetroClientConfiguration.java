c9a0f67fbd7f95b08f0468252f3605daf3574e87
Safi Ahlberg <safi.ahlberg@gmail.com>
2018-07-12 15:32:59 +0200

    A few steps on the way for Metro tests

---
+++ metro/src/test/java/com.service.mm.mailbox.client/MetroClientConfiguration.java
@@ -0,0 +1,25 @@
+package com.service.mm.mailbox.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import se.gov.minameddelanden.service.Mailbox;
+import se.gov.minameddelanden.service.ServicePortV3;
+
+import javax.xml.ws.BindingProvider;
+
+@Configuration
+public class MetroClientConfiguration {
+
+    @Bean(name = "clientv3")
+    public Object generateProxyV3() {
+        Mailbox service = new Mailbox();
+
+        ServicePortV3 port = service.getServicePortV3();
+        BindingProvider bp = (BindingProvider) port;
+        bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:8080/services/mailbox/v3");
+
+        return port;
+    }
+
+
+}
